e30b28400d69a4f99633b7a995222ca868e0900b
Alexander Voronin <aovoronin.piano@gmail.com>
2023-02-03 10:22:28 +0300

    feat: almost understand the mistake

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import PostController from "../PostController";
+import PostController from './PostController.js';
 const router = new Router();
 
 router.post("/posts", PostController.create);
