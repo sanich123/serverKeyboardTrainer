27f4aed4501cde6577b5d44b1667ad40b6ccacc3
sanich123 <aovoronin.piano@gmail.com>
2023-02-15 13:54:04 +0300

    feat: experiments with deployed version

--- router/router.js
+++ router/router.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-// import PostController from '../controller/PostController.js';
 import RaceDataController from '../controller/RaceDataController.js';
 
 const router = new Router();
@@ -8,10 +7,5 @@ router.get('/races/:name', RaceDataController.getAllRaces);
 router.post('/picture', RaceDataController.createPicture);
 router.get('/pictures', RaceDataController.getPictures);
 router.delete('/pictures', RaceDataController.deletePictures);
-// router.post('/posts', PostController.create);
-// router.get('/posts', PostController.getAll);
-// router.get('/posts/:id', PostController.getOne);
-// router.put('/posts/:id', PostController.update);
-// router.delete('/posts/:id', PostController.delete);
 
 export default router;
